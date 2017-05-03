1b8029871cd6e91a29d47f9682b5906d8943d6d6
Light Jiang(姜继伟) <CI11979@careerintlinc.local>
2017-05-03 15:40:42 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!--<div style="height: 5rem"><img style="width: 100%;height: 100%;" src="./assets/logo.png"></div> eg:图片自适应-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello - 副本.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+
+
+    <mt-button type="default" @click.native="tip">mintui button</mt-button>
+    <mt-button type="default" @click.native="handerClick">handerClick button</mt-button>
+
+
+
+
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="1">选项一</mt-tab-item>
+      <mt-tab-item id="2">选项二</mt-tab-item>
+    </mt-navbar>
+
+    <!-- tab-container -->
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <mt-cell v-for="n in array" :title="'内容 ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+      </mt-tab-container-item>
+    </mt-tab-container>
+
+    <div>
+      <mt-button type="default" size="large">点击</mt-button>
+    </div>
+
+
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import { Navbar, TabItem,Toast,mtTabContainerItem } from 'mint-ui';
+
+
+//import { Button } from 'mint-ui';
+
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome',
+      fff: 'sdfs',
+      selected: 'selected',
+      array:5
+
+
+    }
+  },
+  methods: {
+    tip() {
+        Toast('提示信息');
+    },
+    handerClick(){
+    }
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+.dd{
+  font-size: 1.2rem;
+  width: 9.4rem;
+  height: 3rem;
+  display: inline-block;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
