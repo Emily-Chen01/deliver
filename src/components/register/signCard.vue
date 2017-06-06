e6ec380011182efd3157f0d5f7a02673902e1a7d
Light Jiang(姜继伟) <CI11979@careerintlinc.local>
2017-06-06 13:20:01 +0800

    tool

--- src/components/register/signCard.vue
+++ src/components/register/signCard.vue
@@ -86,18 +86,18 @@
       <div class="bottomTool">
         <div style="display: flex;position: relative">
           <div style="flex: 2"  @click="routerTool(1)">
-            <div style="height: 1.8rem;width: 2rem;position: absolute;top: -0.4rem;left: 7rem;">
+            <div style="height: 1.8rem;width: 2rem;position: absolute;top: -0.4rem;left: 23%;">
               <img :src="imgSrc.doIconBlue" class="avatar" v-if="initBlue">
               <img :src="imgSrc.doIcon" class="avatar" v-if="init">
             </div>
-            <div style="height: 1.5rem;position: absolute;left: 7rem;top:2.4rem">工作台</div>
+            <div style="height: 1.5rem;position: absolute;left: 23%;top:2.4rem">工作台</div>
           </div>
           <div style="flex: 2"  @click="routerTool(2)">
-            <div style="height: 1.8rem;width: 2rem;position: absolute;top: -0.4rem;left: 18.5rem;">
+            <div style="height: 1.8rem;width: 2rem;position: absolute;top: -0.4rem;right:29%;">
               <img :src="imgSrc.setIconBlue" class="avatar" v-if="initBlueSet">
               <img :src="imgSrc.setIcon" class="avatar"  v-if="initSet">
             </div>
-            <div style="height: 1.5rem;position: absolute;left: 19rem;top:2.4rem">设置</div>
+            <div style="height: 1.5rem;position: absolute;right:27.5%;top:2.4rem">设置</div>
           </div>
         </div>
       </div>
