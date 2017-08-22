bfdf3c9bf11d630c400bd681e4cf304d19d1fcf0
Yang Di(杨帝) <CI11840@careerintlinc.local>
2017-08-22 10:33:52 +0800

    17.8.22-1 mySalary w optimize

--- src/components/SignIn/mySalary.vue
+++ src/components/SignIn/mySalary.vue
@@ -1,74 +1,45 @@
 <template>
-  <div style="background: #ffffff">
-    <div class="titleHeard">
-      <div class="titleLeft" @click="handleClickUp()">< </div>
-      <div class="titleMiddle">{{year}}年{{this.upTime}}月</div>
-      <div class="titleRight" @click="handleClickDown()"> ></div>
+  <div id="mySalary-wrapper">
+    <!--头部-->
+    <div class="maSalary-header">
+      <p class="maSalary-header-center" v-text="year+'年'+upTime+'月'"></p>
+      <p class="maSalary-header-left" @click="handleClickUp()"><</p>
+      <p class="maSalary-header-right" @click="handleClickDown()">></p>
     </div>
-    <div class="concentClass">
-      <div class="concentClassNr">
-        <div class="concentClassSpan">
-          <div class="concentTop">
-            <div style="text-align: center">
-              实际到账金额：{{dateGrid.money}}
-
-
-
-
-
-            </div>
-          </div>
-          <div class="concentMiddle">
-            <div style="width: 30%;height: 0.05rem;background: #d2dce6;text-align: right;margin-left: 35%"></div>
-          </div>
-          <div class="concentBottom">
-            <div style="width: 100%;">
-              <div style="width: 20%;float: left;">
-                <div style="width: 2rem;height: 1rem;line-height: 1rem;padding-left:30%">
-                  <img :src="imgSrc.qianIcon" class="avatar" style="width: 100%">
-                </div>
-              </div>
-              <div style="width: 58%;float: left;">扣除金额：{{dateGrid.lost}}</div>
-              <div style="width: 20%;float: right;">
-                <div style="width: 2rem;height: 1rem;line-height: 1rem;padding-left:30%">
-                  <img :src="imgSrc.qianIcon" class="avatar" style="width: 100%">
-                </div>
-              </div>
-            </div>
-          </div>
+    <!--内容-->
+    <div class="maSalary-content">
+      <div class="maSalary-total-wrapper">
+        <div class="maSalary-total-box">
+          <h2 v-text="'实际到账金额:'+moneyFormat(dateGrid.money)"></h2>
+          <div class="maSalary-total-line"></div>
+          <h3 v-text="'扣除金额:'+moneyFormat(dateGrid.lost)"></h3>
+          <img :src="imgSrc.qianIcon">
+          <img :src="imgSrc.qianIcon">
         </div>
       </div>
+      <hr/>
+      <!--列表-->
+      <div class="maSalary-details">
+        <table>
+          <thead>
+          <tr>
+            <th>款项</th>
+            <th>扣除金额</th>
+            <th>增加金额</th>
+            <th>小计</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in dateGrid.detail">
+            <td class="maSalary-details-td" v-text="item.remark"></td>
+            <td v-text="moneyFormat(item.deduct)"></td>
+            <td v-text="moneyFormat(item.add)"></td>
+            <td v-text="moneyFormat(item.total)"></td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div class="concentBodyClass">
-      <!--<div class="concentBodyClassTitle">-->
-      <!--<div class="concentBodyClassTitleSpan">款项</div>-->
-      <!--<div class="concentBodyClassTitleSpan">扣除金额</div>-->
-      <!--<div class="concentBodyClassTitleSpan">增加金额</div>-->
-      <!--<div class="concentBodyClassTitleSpan">小计</div>-->
-      <!--</div>-->
-      <!--<div class="concentBodyClassTitle" v-for="item in dateGrid.detail">-->
-      <!--<div class="concentBodyClassTitleSpanColor" style="text-overflow:ellipsis;overflow:hidden">{{item.remark}}</div>-->
-      <!--<div class="concentBodyClassTitleSpanColor">{{item.deduct}}</div>-->
-      <!--<div class="concentBodyClassTitleSpanColor">{{item.add}}</div>-->
-      <!--<div class="concentBodyClassTitleSpanColor">{{item.total}}元</div>-->
-      <!--</div>-->
-
-      <table style="width: 100%;min-height: 3rem;">
-        <tr>
-          <th>款项</th>
-          <th>扣除金额</th>
-          <th>增加金额</th>
-          <th>小计</th>
-        </tr>
-        <tr v-for="item in dateGrid.detail">
-          <td style=" background: #f8fbfb;text-overflow:ellipsis;overflow:hidden">{{item.remark}}</td>
-          <td>{{item.deduct}}</td>
-          <td>{{item.add}}</td>
-          <td>{{item.total}}</td>
-        </tr>
-      </table>
-    </div>
-    <div style="height: 1rem;"></div>
   </div>
 </template>
 <script>
@@ -85,23 +56,15 @@
         downTime: '',
         year: new Date().getFullYear(),
         dateGrid: {
-          money: '',
-          lost: '',
+          money: 0,
+          lost: 0,
           detail: [
-//                 {
-//                   remark:'差旅费',
-//                   deduct: 2000,
-//                   total:1235
-//                 },{
-//                   remark:'加油费',
-//                   deduct: 2000,
-//                   total:5235
-//                 }
-//                 ,{
-//                   remark:'停车费',
-//                   deduct: 2000,
-//                   total:4235
-//                 }
+//            {
+//              remark: '差旅费',
+//              deduct: 0,
+//              add: 34,
+//              total: 1235
+//            }
           ]
         }
       }
@@ -112,45 +75,49 @@
     },
 
     methods: {
+      moneyFormat(money){
+        let Money = money ? ((money >= 0) ? (money + '元') : '无') : '无';
+        return Money;
+      },
+      // 上一个月
       handleClickUp(){
         this.upTime = this.upTime - 1;
-        if (this.upTime == 1) {
+        if (this.upTime < 1) {
           this.upTime = 12;
           this.year = this.year - 1;
         }
-        var geshi = this.upTime < 10 ? '0' + this.upTime : this.upTime;
-        var jian = this.year + '/' + geshi;
-        this.dateGrid = {};
-
-        this.initDate(jian);
+        let minus = this.year + '/' + (this.upTime < 10 ? '0' + this.upTime : this.upTime);
+        this.initDate(minus);
       },
+      // 下一个月
       handleClickDown(){
         this.upTime = this.upTime + 1;
-        if (this.upTime >= 12) {
+        if (this.upTime > 12) {
           this.upTime = 1;
           this.year = this.year + 1;
         }
-        var geshi2 = this.upTime < 10 ? '0' + this.upTime : this.upTime;
-        var add = this.year + '/' + geshi2;
-        this.dateGrid = {};
+        let add = this.year + '/' + ((this.upTime < 10) ? ('0' + this.upTime) : this.upTime);
         this.initDate(add);
       },
       initDate(sum){ //查询当月工资
         let param = {
-          "date": sum
+          date: sum
         };
         this.$http.post('/api/v1.0/client/findMonthSalaryReport', param).then(response => { //参数不传默认当月
-          if (response.body.result) {
-            this.dateGrid = response.body.result;
-          } else {
-            this.dateGrid = {
-              money: '',
-              lost: '',
-              detail: []
-            };
+          if (response.body.code === 200) {
+            this.dateGrid = {};
+            if (response.body.result) {
+              this.dateGrid = response.body.result;
+            } else {
+              this.dateGrid = {
+                money: '',
+                lost: '',
+                detail: []
+              };
+            }
           }
         }, response => {
-          console.log('查询当月工资 error callback');
+//          console.log('查询当月工资 error callback');
         });
       }
     },
@@ -159,122 +126,232 @@
 
 </script>
 
-<style scoped>
-  table {
-    border-collapse: collapse;
-  }
-
-  table, td, th {
-    border: 1px solid #d2dce6;
-    font-size: 1.1rem;
-  }
-
-  th {
-    background: #f8fbfb;
-  }
-
-  .titleHeard {
-    display: flex;
-    background: #26a2ff;
-    height: 3.2rem;
-    line-height: 3.2rem;
-    color: #ffffff;
-  }
-
-  .titleLeft {
-    flex: 1;
-  }
-
-  .titleMiddle {
-    flex: 3;
-  }
-
-  .titleRight {
-    flex: 1;
-  }
-
-  .concentClass {
-    width: 93%;
-    margin: 1rem;
-    height: 8rem;
-    line-height: 8rem;
-    background: rgb(239, 242, 247);
-    position: relative;
-  }
-
-  .concentClassNr {
-    width: 93%;
-    height: 6.8rem;
-    position: absolute;
-    top: 0.5rem;
-    left: 1rem;
-    line-height: 6.8rem;
-    background: rgb(217, 224, 234);
-  }
-
-  .concentClassSpan {
-    width: 93%;
-    height: 5.8rem;
-    position: absolute;
-    top: 0.5rem;
-    left: 0.8rem;
-    line-height: 6.8rem;
-    background: rgb(239, 242, 247);
-  }
-
-  .concentTop {
-    position: absolute;
-    width: 100%;
-    height: 2rem;
-    line-height: 2rem;
-    font-weight: 600;
-  }
-
-  .concentMiddle {
-    position: absolute;
-    width: 96%;
-    left: 2%;
-    height: 0.5rem;
-    line-height: 0.5rem;
-    top: 2.8rem;
-  }
-
-  .concentBottom {
-    position: absolute;
-    width: 100%;
-    height: 2rem;
-    line-height: 2rem;
-    top: 3.5rem;
-    font-size: 0.9rem;
-
-  }
-
-  .concentBodyClass {
-    width: 93%;
-    min-height: 3rem;
-    font-size: 1rem;
-    /*border: 1px solid #d2dce6;*/
-    margin: 1rem;
-  }
-
-  .concentBodyClassTitle {
-    display: flex;
-    height: 3rem;
-    line-height: 3rem;
-    background: #f8fbfb;
-  }
-
-  .concentBodyClassTitleSpan {
-    flex: 2;
-    height: 3rem;
-    line-height: 3rem;
-  }
-
-  .concentBodyClassTitleSpanColor {
-    flex: 1;
-    height: 3rem;
-    line-height: 3rem;
+<style lang="scss">
+  #mySalary-wrapper {
     background: #ffffff;
-    color: #1f2d3d;
+    width: 100%;
+    p {
+      margin: 0;
+    }
+    .maSalary-header {
+      height: 44px;
+      width: 100%;
+      background: #26a2ff;
+      color: #ffffff;
+      .maSalary-header-left, .maSalary-header-right {
+        float: left;
+        width: 40px;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        margin-top: -44px;
+
+      }
+      .maSalary-header-right {
+        float: right;
+      }
+      .maSalary-header-center {
+        width: 100%;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+      }
+    }
+    .maSalary-content {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 15px;
+      .maSalary-total-wrapper {
+        box-sizing: border-box;
+        width: 100%;
+        height: 125px;
+        background: rgb(239, 243, 247);
+        padding: 10px;
+        .maSalary-total-box {
+          box-sizing: border-box;
+          width: 100%;
+          height: 100%;
+          border: 5px solid rgb(217, 224, 233);
+          h2 {
+            margin: 0;
+            height: 50px;
+            line-height: 50px;
+            font-size: 17px;
+          }
+          h3 {
+            margin: 0;
+            height: 43px;
+            line-height: 43px;
+            font-size: 13px;
+          }
+          .maSalary-total-line {
+            margin: 0 auto;
+            height: 0;
+            width: 60px;
+            border: 1px solid rgb(224, 231, 237);
+
+          }
+          img {
+            float: left;
+            margin-top: -43px;
+            margin-left: 10px;
+            width: 40px;
+            height: 33px;
+          }
+          img:last-child {
+            float: right;
+            margin-right: 10px;
+          }
+        }
+      }
+      hr {
+        margin: 15px 0 12px;
+        border: none;
+        height: 0;
+        width: 100%;
+        border-top: 1px solid #d2dce6;
+      }
+      .maSalary-details {
+        width: 100%;
+        table {
+          width: 100%;
+          border-collapse: collapse;
+          border: 1px solid #d2dce6;
+          font-size: 14px;
+          th, td {
+            width: 25%;
+            padding: 10px;
+            border: 1px solid #d2dce6;
+            font-size: 13px;
+          }
+          th, .maSalary-details-td {
+            color: #475669;
+            background: #f8fbfb;
+          }
+          td {
+            color: #1f2d3d;
+            background: #ffffff;
+          }
+        }
+      }
+    }
+
+    /*table {*/
+    /*border-collapse: collapse;*/
+    /*}*/
+
+    /*table, td, th {*/
+    /*border: 1px solid #d2dce6;*/
+    /*font-size: 1.1rem;*/
+    /*}*/
+
+    /*th {*/
+    /*background: #f8fbfb;*/
+    /*}*/
+
+    /*.titleHeard {*/
+    /*display: flex;*/
+    /*background: #26a2ff;*/
+    /*height: 3.2rem;*/
+    /*line-height: 3.2rem;*/
+    /*color: #ffffff;*/
+    /*}*/
+
+    /*.titleLeft {*/
+    /*flex: 1;*/
+    /*}*/
+
+    /*.titleMiddle {*/
+    /*flex: 3;*/
+    /*}*/
+
+    /*.titleRight {*/
+    /*flex: 1;*/
+    /*}*/
+
+    /*.concentClass {*/
+    /*width: 93%;*/
+    /*margin: 1rem;*/
+    /*height: 8rem;*/
+    /*line-height: 8rem;*/
+    /*background: rgb(239, 242, 247);*/
+    /*position: relative;*/
+    /*}*/
+
+    /*.concentClassNr {*/
+    /*width: 93%;*/
+    /*height: 6.8rem;*/
+    /*position: absolute;*/
+    /*top: 0.5rem;*/
+    /*left: 1rem;*/
+    /*line-height: 6.8rem;*/
+    /*background: rgb(217, 224, 234);*/
+    /*}*/
+
+    /*.concentClassSpan {*/
+    /*width: 93%;*/
+    /*height: 5.8rem;*/
+    /*position: absolute;*/
+    /*top: 0.5rem;*/
+    /*left: 0.8rem;*/
+    /*line-height: 6.8rem;*/
+    /*background: rgb(239, 242, 247);*/
+    /*}*/
+
+    /*.concentTop {*/
+    /*position: absolute;*/
+    /*width: 100%;*/
+    /*height: 2rem;*/
+    /*line-height: 2rem;*/
+    /*font-weight: 600;*/
+    /*}*/
+
+    /*.concentMiddle {*/
+    /*position: absolute;*/
+    /*width: 96%;*/
+    /*left: 2%;*/
+    /*height: 0.5rem;*/
+    /*line-height: 0.5rem;*/
+    /*top: 2.8rem;*/
+    /*}*/
+
+    /*.concentBottom {*/
+    /*position: absolute;*/
+    /*width: 100%;*/
+    /*height: 2rem;*/
+    /*line-height: 2rem;*/
+    /*top: 3.5rem;*/
+    /*font-size: 0.9rem;*/
+
+    /*}*/
+
+    /*.concentBodyClass {*/
+    /*width: 93%;*/
+    /*min-height: 3rem;*/
+    /*font-size: 1rem;*/
+    /*!*border: 1px solid #d2dce6;*!*/
+    /*margin: 1rem;*/
+    /*}*/
+
+    /*.concentBodyClassTitle {*/
+    /*display: flex;*/
+    /*height: 3rem;*/
+    /*line-height: 3rem;*/
+    /*background: #f8fbfb;*/
+    /*}*/
+
+    /*.concentBodyClassTitleSpan {*/
+    /*flex: 2;*/
+    /*height: 3rem;*/
+    /*line-height: 3rem;*/
+    /*}*/
+
+    /*.concentBodyClassTitleSpanColor {*/
+    /*flex: 1;*/
+    /*height: 3rem;*/
+    /*line-height: 3rem;*/
+    /*background: #ffffff;*/
+    /*color: #1f2d3d;*/
+    /*}*/
   }
 </style>
--- src/components/register/manyCompany.vue
+++ src/components/register/manyCompany.vue
@@ -1,40 +1,24 @@
 <template>
-  <div>
+  <div id="manyCompanyWarpper">
     <div class="manyCompanyTitle">{{ msg }}</div>
-    <div>
-      <div>
-        <a class="mint-cell mint-cell-lineBottom"
-           v-for="(item , $index) in arryName" @click="handerComeCompany(item,$index)">
-          <div class="mint-cell-title qing">
-            <div class="postionImages">
-              <img :src="imgSrc.ico_company" class="imgSize">
-            </div>
-            <div>
-              <i class="indexicon icon-toast"></i>
-              <span>{{item.compName}}</span></div>
-          </div>
-          <div class="mint-cell-right"></div>
-          <i class="mint-cell-allow-right"></i>
-        </a>
-      </div>
+    <div class="manyCompanyList" v-for="item in arryName" @click="handerComeCompany(item)">
+      <mt-cell :title="item.compName" is-link>
+        <img slot="icon" :src="imgSrc.ico_company" width="16" height="16">
+      </mt-cell>
     </div>
   </div>
-
 </template>
 <script>
-  let fromPath = '';
+  import {Cell} from 'mint-ui';
   export default {
     name: 'manyCompany',
     data() {
       return {
-        uId: '',
         msg: '您的员工信息存在多家公司请选择',
-        arryName: [
-          {
-            name: '苏州工业园区科技有限公司',
-            url: 'www.baidu.com'
-          },
-        ],
+        arryName: [{
+          name: '',
+          url: ''
+        }],
         imgSrc: {
           ico_company: require('../../assets/ico_company.png'),
         },
@@ -43,18 +27,17 @@
     created: function () {
       let phoneObj = {
         phone: this.getCookie('iphoneNumber'),
-      }
+      };
       this.$http.get('/api/v1.0/client/findCompanies/' + phoneObj.phone).then(response => { //初始化查询有没有公司
-        console.log(response, '这是查处的公司接口数据');
-
-        console.log(this.uId);
-        this.arryName = response.body.result
-        if (response.body.code === 500) {
+        if (response.body.code === 200) {
+          this.arryName = response.body.result;
+        } else if (response.body.code === 500) {
           this.$http.post('/api/v1.0/client/unbind').then(response => {
-            console.log('解除绑定');
-            this.$router.push({path: '/index'});
+            if (response.body.code === 200) {
+              this.$router.push({path: '/index'});
+            }
           }, response => {
-            this.$router.push({path: '/index'});
+//            console.log('error callback');
           });
         }
       }, response => {
@@ -62,10 +45,7 @@
       });
     },
     methods: {
-      handerComeCompany(item, fff) {
-
-        console.log('yy' + item.companyUid)
-
+      handerComeCompany(item) {
         let param = {
           companyUid: item.companyUid,
         };
@@ -77,80 +57,33 @@
         }, response => {
           console.log('error callback');
         });
-
       },
-//
     },
-
   }
-
 </script>
-
-<style>
-  .qing {
-    width: 5rem;
-    postion: relative;
-  }
-
-  .qing::before {
-    content: " ------- ";
-    /*content: url(../../assets/ico_leave.png);*/
-    /*font-size: 0.5em;*/
-    /*background-size: 19px 20px;*/
-    /*display: inline-block;*/
-    /*background-color:yellow;*/
-    color: #ffffff;
-    /*font-weight:bold;*/
-    width: 9%;
-    /*height: 20px;*/
-  }
-
-  .postionImages {
-    position: absolute;
-    width: 16px;
-    height: 16px;
-    line-height: 1.8rem;
-    left: 0.9rem;
-    top: 18%;
-  }
-
-  .imgSize {
-    width: 100%;
-    height: 100%
-  }
-
-  .manyCompanyTitle {
-    height: 5rem;
-    line-height: 5rem;
-    /*font-weight: 600;*/
-    color: #8492a6;
-    font-size: 1.4rem;
-  }
-
-  .mint-cell-lineBottom {
+<style lang="scss">
+  #manyCompanyWarpper {
     border-bottom: 1px solid #d9d9d9;
-  }
+    .manyCompanyTitle {
+      height: 50px;
+      line-height: 50px;
+      color: #8492a6;
+      font-size: 16px;
+    }
+    .manyCompanyList {
+      // 复写组件样式
+      .mint-cell {
+        text-align: left;
+        background-position: top;
+        .mint-cell-title {
 
-  .mint-cell-title {
-    /*background-image: -webkit-linear-gradient(top, #d9d9d9, #d9d9d9 50%, transparent 0);*/
-    /*background-image: linear-gradient(180deg, #d9d9d9, #d9d9d9 50%, transparent 0);*/
-    /*background-size: 120% 1px;*/
-    /*background-repeat: no-repeat;*/
-    /*background-position: 0 0;*/
-    /*background-origin: content-box;*/
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    box-sizing: border-box;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    font-size: 16px;
-    line-height: 1;
-    min-height: inherit;
-    overflow: hidden;
-    padding: 0 10px;
-    width: 100%;
+          padding: 0 10px;
+          span {
+            margin-left: 10px;
+            color: #1f2d3d;
+          }
+        }
+      }
+    }
   }
-
 </style>
